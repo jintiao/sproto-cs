b4210cd6453561b2e69b26d3509df6e7ed97e332
unknown <jinyao.liang@beiwks0296.bei.gameloft.org>
2014-09-04 11:42:28 +0800

    fix compile errors in vs 2010

---
+++ test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sproto_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpTest.Run();
+        }
+    }
+}
